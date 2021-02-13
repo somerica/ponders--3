cd5d2f51d1e11cb739cd4f4460acdf9f31cedf2e
Somer Jackson <somer.jackson@gmail.com>
2021-02-13 13:55:17 -0500

    added navBar

---
+++ client/src/components/NavBar/NavBar.jsx
@@ -0,0 +1,15 @@
+import React from 'React';
+
+const NavBar = () => {
+    return(
+        <div class="nav-wrapper">
+      <a href="#" class="brand-logo">Ponders Cleaners</a>
+      <ul id="nav-mobile" class="right hide-on-med-and-down">
+        <li><a href="sass.html">Services</a></li>
+        <li><a href="badges.html">Contact</a></li>
+        <li><a href="collapsible.html">Pick-Up</a></li>
+        <li><a href="collapsible.html">Admin</a></li>
+      </ul>
+    </div>
+    )
+}
