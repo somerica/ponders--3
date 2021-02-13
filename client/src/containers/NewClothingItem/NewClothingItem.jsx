630286263df51b9f9683814b52c56e836573a44a
Somer Jackson <somer.jackson@gmail.com>
2021-02-13 15:38:18 -0500

    Update NewClothingItem.jsx

--- client/src/containers/NewClothingItem/NewClothingItem.jsx
+++ client/src/containers/NewClothingItem/NewClothingItem.jsx
@@ -1,9 +1,9 @@
 import React from 'React';
 
-// const NewClothingItem = () => {
-//     return(
-//         <div>yes</div>
-//     )
-// }
+ const NewClothingItem = () => {
+     return(
+        <div>yes</div>
+     )
+ }
 
-// export default NewClothingItem;
+ export default NewClothingItem;
