76540b923380a42170d930c1f3baeca09c4e78eb
Somer Jackson <somer.jackson@gmail.com>
2021-02-14 22:21:20 -0500

    added form to admin page

--- client/src/containers/Admin/Admin.jsx
+++ client/src/containers/Admin/Admin.jsx
@@ -2,8 +2,44 @@ import React from 'react';
 
 const Admin = () => {
     return(
-        <div>yes</div>
-    )
-}
+        <div className="container">
+      <div className="row">
+        <div className="col">
+          <h1 className="center-align">Add New Clothing Item</h1>
+        </div>
+      </div>
+      <div className="row">
+        <form action="">
+
+          <div className="row">
+            <div className="input-field col s6">
+              <input placeholder="Clothing Item" id="item" type="text" />
+              <label htmlFor="title">Clothing Item</label>
+            </div>
+            <div className="input-field col s6">
+              <input placeholder="Clothing Price" id="price" type="text" />
+              <label htmlFor="price">Clothing Price</label>
+            </div>
+          </div>
+
+          <div className="row">
+          <div class="input-field col s12">
+            <input placeholder="Clothing description" id="description" type="text"/>
+            <label htmlFor="description">Clothing Description</label>
+          </div>
+          </div>
+          <div class="row">
+          <div class="input-field col s12">
+             <input placeholder= "Clothing Image" id="imageURL" type="text"/>
+            <label htmlFor="imageURL">Clothing Image</label>
+          </div>
+         </div>
+        </form>
+      </div>
+    </div>
+  );
+};
+    
+
 
 export default Admin;
--- client/src/containers/NewClothingItem/NewClothingItem.jsx
+++ client/src/containers/NewClothingItem/NewClothingItem.jsx
@@ -1,9 +1,37 @@
-import React from 'react';
+import React from "react";
 
- const NewClothingItem = () => {
-     return (
-        <div>yes</div>
-     )
- }
+const NewClothingItem = () => {
+  return (
+    <div className="container">
+      <div className="row">
+        <div className="col">
+          <h1 className="center-align">Add New Clothing Item</h1>
+        </div>
+      </div>
+      <div className="row">
+        <form action="">
 
- export default NewClothingItem;
+          <div className="row">
+            <div className="input-field col s6">
+              <input placeholder="Clothing Item" id="item" type="text" />
+              <label htmlFor="title">Clothing Item</label>
+            </div>
+            <div className="input-field col s6">
+              <input placeholder="Clothing Price" id="price" type="text" />
+              <label htmlFor="price">Clothing Price</label>
+            </div>
+          </div>
+
+          <div className="row">
+          <div class="input-field col s12">
+            <input placeholder="Clothing description" id="description" type="text"/>
+            <label htmlFor="description">Clothing Description</label>
+          </div>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default NewClothingItem;
