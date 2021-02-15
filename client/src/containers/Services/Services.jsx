d51b08434556f5af3b3a32980dacfb5e96667510
Somer Jackson <somer.jackson@gmail.com>
2021-02-15 18:16:42 -0500

    added options for pricing

--- client/src/containers/Services/Services.jsx
+++ client/src/containers/Services/Services.jsx
@@ -1,44 +1,42 @@
 import React from "react";
+import M from "materialize-css";
+import { Select } from "react-materialize";
 
 const Services = () => {
   return (
     <div class="container">
-        <div className="row">
-      <div class="input-field col s12 m6">
-        <select class="icons">
-          <option value="" disabled selected>
-            Choose your option
-          </option>
-          <option value="" data-icon="images/sample-1.jpg">
-            example 1
-          </option>
-          <option value="" data-icon="images/office.jpg">
-            example 2
-          </option>
-          <option value="" data-icon="images/yuna.jpg">
-            example 3
-          </option>
-        </select>
-        <label>Images in select</label>
-      </div>
-      <div class="input-field col s12 m6">
-        <select class="icons">
-          <option value="" disabled selected>
-            Choose your option
-          </option>
+      <h1>Dry Cleaning vs Laundry?</h1>
+      <p>
+        In dry cleaning we use a process that uses liquids other than water to
+        clean clothes and other types of fabrics.
+      </p>
+      <div className="row">
+        <div class="input-field col s12 m6">
+          <Select value="">
+            <option value="">Dry Clean Prices</option>
+          </Select>
+          <label>Images in select</label>
+        </div>
+        <div class="input-field col s12 m6">
+          <Select>
+            <option value="">Laundry Prices</option>
+          <select class="icons">
           <option value="" data-icon="images/sample-1.jpg" class="left">
-            example 1
+            dress shirts
           </option>
           <option value="" data-icon="images/office.jpg" class="left">
-            example 2
+            pants
           </option>
           <option value="" data-icon="images/yuna.jpg" class="left">
-            example 3
+            jeans
           </option>
-        </select>
-        <label>Images in select</label>
+
+          <label>Images in select</label>
+          </select>
+          </Select>
+          
+        </div>
       </div>
-    </div>
     </div>
   );
 };
