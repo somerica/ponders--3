2e2c54ddecca3fc7a0e6c255bb11704d1b6869ed
Somer Jackson <somer.jackson@gmail.com>
2021-02-15 15:21:54 -0500

    styling home page

--- client/src/containers/Home/Home.jsx
+++ client/src/containers/Home/Home.jsx
@@ -1,37 +1,42 @@
-import React from 'react';
+import React from "react";
 
 const Home = () => {
-    return (
-        
-
-     <div className="row">
-        <div className="col s12 m7">
+  return (
+    <div className="container">
+      <div className="row">
+        <div className="col s3">
           <div className="card">
             <div className="card-image">
-              <img src="images/sample-1.jpg"/>
-              <span className="card-title">Card Title</span>
+              <img src="images/sample-1.jpg" />
             </div>
             <div className="card-content">
-              <p>I am a very simple card. I am good at containing small bits of information.
-              I am convenient because I require little markup to use effectively.</p>
-            </div>
-            <div className="col s12 m7">
-            <div className="card">
-            <div classN="card-image">
-              <img src="images/sample-1.jpg"/>
-              <span className="card-title">Card Title</span>
-            </div>
-            <div className="card-content">
-              <p>I am a very simple card. I am good at containing small bits of information.
-              I am convenient because I require little markup to use effectively.</p>
+              <p>
+                I am a very simple card. I am good at containing small bits of
+                information. I am convenient because I require little markup to
+                use effectively.
+              </p>
             </div>
           </div>
         </div>
+        <div className="col s6">MIDDLE STUFF GOES HERE</div>
+        <div className="col s3">
+          <div className="card">
+            <div className="card-image">
+              <img src="images/sample-1.jpg" />
+            </div>
+            <div className="card-content">
+              <p>
+                I am a very simple card. I am good at containing small bits of
+                information. I am convenient because I require little markup to
+                use effectively.
+              </p>
+            </div>
+          </div>
+          RIGHT STUFF GOES HERE
         </div>
-     </div>
-     </div>
-        
-    )
-}
+      </div>
+    </div>
+  );
+};
 
 export default Home;
