95921bc508d478a72637c72b2b968e318f3b643a
Somer Jackson <somer.jackson@gmail.com>
2021-02-17 13:01:52 -0500

    added data to schema

--- models/clothing-models.js
+++ models/clothing-models.js
@@ -1,15 +1,35 @@
 const mongoose = require('mongoose')
+
 const Schema = mongoose.Schema 
 
-const Clothing = new Schema(
-{
-    item:{type: String ,required:true },
-    price:{type: Number,required:true },
-    detail:{type: String, required: true },
-
-
+const Clothingschema = new Schema({
+title: {
+    type: String,
+    trim:true,
+    required: "Add new Item",
+},
+description:{
+     trim: String,
+    required:true,
+},
+price:{
+    type: Number,
+    required: "Enter a clothing price",
+},
+imageURL:{
+    type: String,
+    trim:true,
+    required: "Enter clothing image",
+},
+category:{
+    type:String,
+    trim: true,
+    required:"Enter a clothing category",
 },
 
-)
+
+});
+
+const Clothing = mongoose.model("Transaction" ,clothingSchema);
 
 module.exports= mongoose.model('clothing', Clothing)
